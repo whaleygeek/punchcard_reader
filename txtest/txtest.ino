983546b895dd72599425fe13f62f5c8a115d8c71
David Whale <david@thinkingbinaries.com>
2016-05-08 18:32:47 +0100

    Software driven UART is logically correct. Counter overflow interrupt doesn't seem to work yet

--- txtest/txtest.ino
+++ txtest/txtest.ino
@@ -6,22 +6,55 @@
 #define LED_GREEN 0  // PB0, pin 5
 #define TX 1         // PB1, pin 6
 
+#define HALF_BIT_TIME 5 //ms
+
 void setup()
 {
   pinMode(LED_RED, OUTPUT);
   pinMode(LED_GREEN, OUTPUT);
   pinMode(TX, OUTPUT);
-  //Serial.begin(9600);
+  digitalWrite(TX, HIGH); // default state=marking
   digitalWrite(LED_RED, HIGH);
+  
+  USICR = B00010000;
+  // start condition interrupt: off
+  // counter overflow interrupt: off
+  // wire mode: 01 (three wire)
+  // clock source: USICLK generated
+  // USICLK: 0 
+  // toggle clock: no
 }
 
 void loop()
 {
+  byte i;
   digitalWrite(LED_GREEN, HIGH);
-  //Serial.println('hello');
-  digitalWrite(TX, HIGH);
-  delay(10);
-  digitalWrite(TX, LOW);
+  
+  // Transmit a single byte using USI
+  // Transmit start bit (low)
+  USIDR = 0x00; // high bit zero
+  // clock out start bit
+  delay(HALF_BIT_TIME*2);
+  
+  // Clock out 8 data bits
+  USIDR = 0xAA;
+  // use counter, so that later we can use timer counter hardware for accurate timing
+  USISR = B00001000; // overflow interrupt disabled, clock preloaded with 8 (+8=overflow)
+  //do
+  for (i=0; i<8; i++)
+  {  
+    delay(HALF_BIT_TIME);
+    USICR |= B00000010; //USICLK;
+    delay(HALF_BIT_TIME);
+    USICR &= ~ B00000010; //USICLK;
+  }
+  //while ((USICR & B01000000) == 0);
+  
+  // Clock out stop bit (high)
+  USIDR = 0x80;
+  delay(HALF_BIT_TIME*2);
+  
   digitalWrite(LED_GREEN, LOW);
   delay(100);
+  
 }
